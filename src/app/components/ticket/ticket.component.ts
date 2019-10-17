1adb1785e5d28867a158fadc6ceb83eb12e89fd1
sudhir600 <sudhirgupta.456@gmail.com>
2019-10-17 15:36:39 +0530

    How to impliment common lib file for all component ngIf, ngFor, ngSwitch ngModel and click function demo

--- src/app/components/ticket/ticket.component.ts
+++ src/app/components/ticket/ticket.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import common from '../../lib/common'
 
 @Component({
   selector: 'app-ticket',
   templateUrl: './ticket.component.html',
   styles: []
 })
-export class TicketComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class TicketComponent {
+	title: string = "some String goes here";
+  token: any = common.token;
+  names = ['momah', 'roochit', 'kumar', 'shayam'];
+  selectedValue: string= 'One';
+  items = [
+            {name: 'One', val: 1},
+            {name: 'Two', val: 2},
+            {name: 'Three', val: 3}
+          ];
+  alertMe(){
+    alert(1)
   }
-
 }
