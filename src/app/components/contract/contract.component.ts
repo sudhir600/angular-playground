5887f755c51a36b33cf225cb2c5ab67e801f1cc1
sudhir600 <sudhirgupta.456@gmail.com>
2019-10-18 13:06:07 +0530

    ajax (node-fetch)

--- src/app/components/contract/contract.component.ts
+++ src/app/components/contract/contract.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import fetch from 'node-fetch';
 
 @Component({
   selector: 'app-contract',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class ContractComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {}
+  ngOnInit() {}
+  contracts: any = ''
+  gitUserName: any = 'sudhir600'
+  ajax(){
+    return fetch('https://api.github.com/users/github')
+    .then(res => res.json())
+    .then(json => {
+      console.log(json);
+      //return this.contracts = JSON.stringify(json)
+      return this.contracts = json
+    })
   }
-
+  gitUserDeails(){
+    if(!this.gitUserName){
+      alert('invalid Git user name')
+      return
+    }
+    return fetch(`https://api.github.com/users/${this.gitUserName}`)
+    .then(res => res.json())
+    .then(json => {
+      console.log(json);
+      //return this.contracts = JSON.stringify(json)
+      return this.contracts = json
+    })
+  }
 }
